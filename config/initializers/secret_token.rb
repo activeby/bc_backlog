59684f0e29a9eeef23818aa49aae32ef76b4c6c8
Alexander Randa <randa.alex@active.by>
2011-10-22 19:11:37 +0300

    rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Backlog::Application.config.secret_token = '0bb0a86b669338c255546c3c8286ec173cadccf969ffd671d36f826063f83e1f4325ef4c55be35472583ae3a7fbbbfdceea1bb743a2ba5aedb1854d3b0b3a19e'

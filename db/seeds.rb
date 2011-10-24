566cf9f4c17420d099106d2784bbb7651b12b28c
Alexander Randa <randa.alex@active.by>
2011-10-25 00:33:34 +0300

    added seeds for titles of backlog

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Task.create(:uid => 1, :project => nil, :message => '-- 1-Must Have --', :stakeholder => nil, :time => nil)
+Task.create(:uid => 2, :project => nil, :message => '-- 2-Should Have --', :stakeholder => nil, :time => nil)
+Task.create(:uid => 3, :project => nil, :message => '-- 3-Nice to Have --', :stakeholder => nil, :time => nil)
+Task.create(:uid => 4, :project => nil, :message => '-- 4-Don\'t Need Now --', :stakeholder => nil, :time => nil)
+Task.create(:uid => 5, :project => nil, :message => '-- Not Prioritized --', :stakeholder => nil, :time => nil)
